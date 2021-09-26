bc5ad0a3e9c3a772cc08522c35d3f16f182e9b8a
Hoang Dao <nguyendanghoangdao1902@gmail.com>
2021-09-26 15:44:35 +0700

    delete

--- src/main/java/Play.java
+++
@@ -1,6 +0,0 @@
-public class Play {
-    public static void main(String[] ags) {
-        DictionaryCommandLine dictionary = new DictionaryCommandLine();
-        dictionary.dictionnaryBasic();
-    }
-}
