3df43bdccbb7ca37c574de819e98b1e4051dbb00
Hoang Dao <nguyendanghoangdao1902@gmail.com>
2021-09-26 16:05:07 +0700

    delete not use import

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,4 @@
-import java.util.Formatter;
-import java.util.Locale;
-
-public class MainClass {
+public class Main {
     public static void main(String[] args) {
         DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
         dictionaryCommandline.dictionaryBasic();
