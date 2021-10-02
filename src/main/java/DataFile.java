b8b11cdf51128e737050f88f513f1487d2364500
Dinh Cuong <cuong2k2hanam@gmail.com>
2021-10-02 16:06:32 +0700

    version 3.2

---
+++ src/main/java/DataFile.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class DataFile {
+
+    /**
+     * Scan data from file.
+     */
+    public ArrayList<Word> insertFromFile(String file) {
+        ArrayList<Word> arrWord = new ArrayList<Word>(13000);
+//        String file = "E:\\Java\\Dictonary_Assignment\\src\\main\\java\\dictionaries.txt";
+        try (FileInputStream fs = new FileInputStream(file)) {
+            Scanner scanner = new Scanner(fs);
+            while (scanner.hasNextLine()) {
+                Word word = new Word();
+                String a = scanner.next();
+                String b = scanner.nextLine();
+                word.setWord_target(a);
+                word.setWord_explain(b);
+                arrWord.add(word);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Collections.sort(arrWord);
+        return arrWord;
+    }
+
+    /**
+     * Save data to file.
+     */
+    public void saveToFile(String file, ArrayList<Word> arrWorld) {
+
+        File f = new File(file);
+        f.delete();
+        boolean newFile;
+        try{
+            newFile = f.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileWriter fw = new FileWriter(file)) {
+            for(Word w : arrWorld) {
+                fw.write(w.getWord_target() + "\t" + w.getWord_explain() + "\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/DictionaryCommandLine.java
+++ src/main/java/DictionaryCommandLine.java
@@ -1,50 +1,56 @@
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class DictionaryCommandLine extends DictionaryManagement {
+public class DictionaryCommandLine {
+
+    DictionaryManagement dictionaryManagement = new DictionaryManagement();
+
+    public void start() {
+        dictionaryManagement.start();
+    }
+
+    public void end() {
+        dictionaryManagement.end();
+    }
 
     /**
      * Hàm showAllWords() có chức năng hiển thị kết quả danh sách dữ liệu từ điển trên màn hình.
      */
     public void showAllWorlds(){
+        ArrayList<Word> arrWord = dictionaryManagement.arrWord;
         System.out.printf("Number %-20s%-50s\n","English","Vietnamese");
-        for(int i = 0; i < arrWorld.size(); i++) {
-            System.out.printf("%-7d%-20s%-50s\n",i,arrWorld.get(i).getWord_target(),arrWorld.get(i).getWord_explain());
+        for(int i = 0; i < arrWord.size(); i++) {
+            System.out.printf("%-7d%-20s%-50s\n",i, arrWord.get(i).getWord_target(), arrWord.get(i).getWord_explain());
         }
     }
 
-    /**
-     * Hàm dictionaryBasic() có chức năng gọi hàm insertFromCommandline() và showAllWords()
-     * hàm dictionaryAdvanced() có chức năng gọi hàm insertFromFile(), showAllWords() và dictionaryLookup()
-     */
-    public void dictionnaryBasic() {
-        try {
-            insertFromFile();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            insertFromCommandLine();
-        } catch (Exception e) {
-            System.out.println(e.getCause().toString());
-        }
-    }
 
     public void dictionaryAdvanced() throws FileNotFoundException {
-        insertFromFile();
         showAllWorlds();
-        dictionaryLookup();
+        dictionaryManagement.dictionaryLookup();
     }
 
     /**
      * hàm dictionarySearcher() có chức năng tìm kiếm các từ.
      */
     public void dictionarySearcher() {
-        try {
-            insertFromFile();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        String target = "---";
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("Enter charaters you want: ");
+            target = scanner.nextLine();
+            if(target.equals("")) {
+                break;
+            }
+            dictionaryManagement.dictionarySearcher(target);
         }
-        dictionaryLookup();
     }
 
+    /**
+     *
+     */
+    public void insertFromCommandline() {
+        dictionaryManagement.insertFromCommandline();
+    }
 }
